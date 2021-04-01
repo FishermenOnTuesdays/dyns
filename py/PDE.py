0cddb0ea739a276efaf188c54dd4278b568765cd
rouol <karrauln@yandex.ru>
2021-04-01 14:19:52 +0300

    PDEs first tests

---
+++ py/PDE.py
@@ -0,0 +1,95 @@
+"""Temperature evolution in a rod, computed by a ForwardEuler method."""
+
+from numpy import linspace, zeros, linspace, asarray
+import time
+
+
+def ode_FE(f, U_0, dt, T):
+    N_t = int(round(float(T) / dt))
+    # Ensure that any list/tuple returned from f_ is wrapped as array
+    f_ = lambda u, t: asarray(f(u, t))
+    u = zeros((N_t + 1, len(U_0)))
+    t = linspace(0, N_t * dt, len(u))
+    u[0] = U_0
+    for n in range(N_t):
+        u[n + 1] = u[n] + dt * f_(u[n], t[n])
+    return u, t
+
+
+def rhs(u, t):
+    N = len(u) - 1
+    rhs = zeros(N + 1)
+    rhs[0] = dsdt(t)
+    for i in range(1, N):
+        rhs[i] = (beta / dx ** 2) * (u[i + 1] - 2 * u[i] + u[i - 1]) + g(x[i], t)
+    i = N
+    rhs[i] = (beta / dx ** 2) * (2 * u[i - 1] + 2 * dx * dudx(t) - 2 * u[i]) + g(x[N], t)
+    return rhs
+
+
+def dudx(t):
+    return 0
+
+
+def s(t):
+    return 323
+
+
+def dsdt(t):
+    return 0
+
+
+def g(x, t):
+    return 0
+
+
+beta = 8.2E-5
+N = 10
+x = linspace(0, 0.5, N + 1)
+dx = x[1] - x[0]
+# u = zeros(N+1)
+
+# setting initial curve
+U_0 = zeros(N + 1)
+U_0[0] = s(0)
+U_0[1:] = 283
+#
+
+dt = dx ** 2 / (2 * beta)
+print('stability limit:', dt)
+# dt = 0.00034375
+
+t0 = time.process_time()
+u, t = ode_FE(rhs, U_0, dt, T=1 * 60 * 60)
+t1 = time.process_time()
+print('CPU time: %.1fs' % (t1 - t0))
+
+# Make movie
+# import os
+# os.system('rm tmp_*.png')
+import matplotlib.pyplot as plt
+
+plt.ion()
+y = u[0, :]
+lines = plt.plot(x, y)
+plt.axis([x[0], x[-1], 273, s(0) + 10])
+plt.xlabel('x')
+plt.ylabel('u(x,t)')
+'''
+counter = 0
+# Plot each of the first 100 frames, then increase speed by 10x
+change_speed = 100
+for i in range(0, u.shape[0]):
+    print(t[i])
+    plot = True if i <= change_speed else i % 10 == 0
+    lines[0].set_ydata(u[i, :])
+    if i > change_speed:
+        plt.legend(['t=%.0f 10x' % t[i]])
+    else:
+        plt.legend(['t=%.0f' % t[i]])
+    plt.draw()
+    if plot:
+        plt.savefig('tmp_%04d.png' % counter)
+        counter += 1
+    #time.sleep(0.2)
+'''
